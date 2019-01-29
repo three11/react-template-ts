329223f3bd787a71a5dae4713b1a15cbc6a38d27
Atanas Atanasov <scriptex.bg@gmail.com>
2019-01-29 11:17:41 +0200

    Add types definitions to sagas

--- src/containers/Home/sagas.ts
+++ src/containers/Home/sagas.ts
@@ -1,22 +1,35 @@
-import { put, takeEvery, delay } from 'redux-saga/effects';
+import {
+	put,
+	delay,
+	takeEvery,
+	SimpleEffect,
+	PutEffectDescriptor,
+	CallEffectDescriptor,
+	ForkEffectDescriptor
+} from 'redux-saga/effects';
 
+import { CounterAction } from './reducer';
 import { increment, decrement } from './actions';
 import { INCREMENT_ASYNC, DECREMENT_ASYNC } from './constants';
 
-export function* incrementAsync() {
+export function* incrementAsync(): IterableIterator<
+	SimpleEffect<'PUT', PutEffectDescriptor<CounterAction>> | SimpleEffect<'CALL', CallEffectDescriptor>
+> {
 	yield delay(1000);
 	yield put(increment());
 }
 
-export function* decrementAsync() {
+export function* decrementAsync(): IterableIterator<
+	SimpleEffect<'PUT', PutEffectDescriptor<CounterAction>> | SimpleEffect<'CALL', CallEffectDescriptor>
+> {
 	yield delay(1000);
 	yield put(decrement());
 }
 
-export function* incrementSaga() {
+export function* incrementSaga(): IterableIterator<SimpleEffect<'FORK', ForkEffectDescriptor>> {
 	yield takeEvery(INCREMENT_ASYNC, incrementAsync);
 }
 
-export function* decrementSaga() {
+export function* decrementSaga(): IterableIterator<SimpleEffect<'FORK', ForkEffectDescriptor>> {
 	yield takeEvery(DECREMENT_ASYNC, decrementAsync);
 }
