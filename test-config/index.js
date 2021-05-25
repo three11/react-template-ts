38e5a9f7b0584c78a959977bf0e7b1f83734ff34
Atanas Atanasov <scriptex.bg@gmail.com>
2021-05-25 10:13:05 +0300

    Update dependencies (#1693)

--- test-config/index.js
+++ test-config/index.js
@@ -1,6 +1,8 @@
 // @ts-nocheck
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+// TODO: Start using the official adapter when it is finalized
+// https://github.com/enzymejs/enzyme/issues/2429#issuecomment-679265564
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({
 	adapter: new Adapter()
