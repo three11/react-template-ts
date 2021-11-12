b6f6a81fd1354ba8910b9de02b691443939c27aa
Atanas Atanasov <scriptex.bg@gmail.com>
2021-11-12 16:51:53 +0200

    Upgrade packages (#1929)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 const { join, resolve } = require('path');
 
 const webpack = require('webpack');
@@ -9,10 +11,12 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const PrerenderSPAPlugin = require('@dreysolano/prerender-spa-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const r = resolve.bind(__dirname);
 const { mode } = argv;
+const isDev = mode === 'development';
 
 const dotenv = require('dotenv').config({
 	path: join(__dirname, '.env')
@@ -36,17 +40,30 @@ const PATHS = {
 
 const tsConfig = {
 	test: /\.tsx?$/,
-	use: [
-		{
-			loader: 'ts-loader',
-			options: {
-				transpileOnly: true,
-				compilerOptions
-			}
-		}
-	],
+	include: PATHS.src,
 	exclude: PATHS.nodeModules,
-	include: PATHS.src
+	use: {
+		loader: 'ts-loader',
+		options: {
+			transpileOnly: true,
+			compilerOptions: {
+				...compilerOptions,
+				...(isDev
+					? {
+							jsx: 'react-jsxdev',
+							module: 'ESNext'
+					  }
+					: {})
+			},
+			...(isDev
+				? {
+						getCustomTransformers: () => ({
+							before: [ReactRefreshTypeScript()]
+						})
+				  }
+				: {})
+		}
+	}
 };
 
 const htmlConfig = {
@@ -101,245 +118,191 @@ const cssConfig = {
 
 const fontsConfig = {
 	test: /\.(eot|otf|ttf|woff|woff2)$/,
-	use: 'file-loader'
+	type: 'asset'
 };
 
 const svgConfig = {
 	test: /\.svg$/,
-	issuer: /\.tsx?$/,
-	exclude: /node_modules/,
-	loader: 'svg-inline-loader'
+	type: 'asset',
+	issuer: /\.tsx?$/
 };
 
 const svgCSSConfig = {
 	test: /\.svg$/,
-	issuer: /\.s?css?$/,
-	use: [
-		{
-			loader: 'url-loader',
-			options: {
-				limit: 10 * 1024
-			}
-		}
-	]
+	type: 'asset/inline',
+	issuer: /\.s?css?$/
 };
 
 const imagesConfig = {
 	test: /\.(jpg|png|gif|ico)$/,
-	use: [
-		{
-			loader: 'url-loader',
-			options: {
-				limit: 10 * 1024
-			}
-		},
-		{
-			loader: 'image-webpack-loader',
-			options: {
-				mozjpeg: {
-					enabled: true,
-					progressive: true
-				},
-				gifsicle: {
-					interlaced: false
-				},
-				optipng: {
-					optimizationLevel: 7
-				},
-				pngquant: {
-					quality: [0.65, 0.9],
-					speed: 4
-				}
-			}
-		}
-	]
+	type: 'asset'
 };
 
 const mediaConfig = {
 	test: /\.(mp3|mp4|webm)$/,
-	use: {
-		loader: 'url-loader',
-		options: {
-			limit: 10000
-		}
-	}
+	type: 'asset'
 };
 
-module.exports = () => {
-	const isDev = mode === 'development';
-
-	if (isDev) {
-		tsConfig.use.splice(0, 0, {
-			loader: require.resolve('babel-loader'),
-			// @ts-ignore
-			options: {
-				plugins: [require.resolve('react-refresh/babel')]
+module.exports = {
+	mode,
+	entry: ['./src/index.tsx'],
+	output: {
+		path: PATHS.dist,
+		filename: isDev ? '[name].js' : '[name].[chunkhash].bundle.js',
+		sourceMapFilename: isDev ? '[name].bundle.map' : '[name].[chunkhash].bundle.map',
+		chunkFilename: isDev ? '[name].chunk.js' : '[name].[chunkhash].chunk.js',
+		publicPath: '/'
+	},
+	module: {
+		rules: [tsConfig, htmlConfig, cssConfig, fontsConfig, svgConfig, svgCSSConfig, imagesConfig, mediaConfig]
+	},
+	resolve: {
+		alias: {
+			'@src': PATHS.src,
+			'@root': PATHS.root,
+			'@i18n': PATHS.i18n,
+			'@store': PATHS.store,
+			'@assets': PATHS.assets,
+			'@utilities': PATHS.utilities,
+			'@components': PATHS.components,
+			'@containers': PATHS.containers
+		},
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+		modules: ['src', 'node_modules']
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				...dotenv.parsed,
+				NODE_ENV: JSON.stringify(isDev ? 'development' : 'production')
 			}
-		});
-	}
-
-	return {
-		mode,
-		entry: ['./src/index.tsx'],
-		output: {
-			path: PATHS.dist,
-			filename: isDev ? '[name].js' : '[name].[chunkhash].bundle.js',
-			sourceMapFilename: isDev ? '[name].bundle.map' : '[name].[chunkhash].bundle.map',
-			chunkFilename: isDev ? '[name].chunk.js' : '[name].[chunkhash].chunk.js',
-			publicPath: '/'
-		},
-		module: {
-			rules: [tsConfig, htmlConfig, cssConfig, fontsConfig, svgConfig, svgCSSConfig, imagesConfig, mediaConfig]
-		},
-		resolve: {
-			alias: {
-				'@src': PATHS.src,
-				'@root': PATHS.root,
-				'@i18n': PATHS.i18n,
-				'@store': PATHS.store,
-				'@assets': PATHS.assets,
-				'@utilities': PATHS.utilities,
-				'@components': PATHS.components,
-				'@containers': PATHS.containers
-			},
-			extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-			modules: ['src', 'node_modules']
-		},
-		plugins: [
-			new webpack.DefinePlugin({
-				'process.env': {
-					...dotenv.parsed,
-					NODE_ENV: JSON.stringify(isDev ? 'development' : 'production')
-				}
-			}),
-			new HtmlWebPackPlugin({
-				template: './src/index.html',
-				filename: './index.html',
-				inject: true,
-				...(isDev
-					? {}
-					: {
-							minify: {
-								removeComments: true,
-								collapseWhitespace: true,
-								removeRedundantAttributes: true,
-								useShortDoctype: true,
-								removeEmptyAttributes: true,
-								removeStyleLinkTypeAttributes: true,
-								keepClosingSlash: true,
-								minifyJS: true,
-								minifyCSS: true,
-								minifyURLs: true
-							}
-					  })
-			}),
-			// @ts-ignore
-			new LoadablePlugin(),
-			new CopyWebpackPlugin({
-				// @ts-ignore
-				patterns: [
-					{
-						from: 'src/assets/',
-						to: 'assets/'
-					}
-				]
-			}),
-			new MiniCssExtractPlugin({
-				filename: isDev ? '[name].css' : '[name].[hash].css',
-				chunkFilename: isDev ? '[id].css' : '[id].[hash].css'
-			}),
+		}),
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+			inject: true,
 			...(isDev
-				? [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]
-				: [
-						new WebpackPwaManifest({
-							name: process.env.APP_NAME,
-							short_name: process.env.APP_SHORT_NAME,
-							description: process.env.APP_DESCRIPTION,
-							background_color: process.env.APP_BACKGROUND_COLOR,
-							theme_color: process.env.APP_THEME_COLOR,
-							inject: true,
-							ios: true,
-							icons: [
-								{
-									src: r(process.env.APP_ICON),
-									sizes: [72, 96, 128, 144, 192, 384, 512]
-								},
-								{
-									src: r(process.env.APP_ICON),
-									sizes: [120, 152, 167, 180],
-									ios: true
-								}
-							]
-						}),
-						new PrerenderSPAPlugin({
-							staticDir: join(__dirname, 'dist'),
-							// @ts-ignore
-							routes: Object.values(Routes)
-						})
-				  ])
-		],
-		cache: true,
-		bail: false,
-		target: 'web',
-		devtool: isDev ? 'eval-source-map' : false,
-		devServer: {
-			hot: true,
-			noInfo: true,
-			contentBase: './dist',
-			historyApiFallback: true
-		},
-		stats: 'errors-only',
-		performance: {
-			hints: false
-		},
-		optimization: isDev
-			? {
-					splitChunks: {
-						chunks: 'all'
-					}
-			  }
-			: {
-					minimize: true,
-					minimizer: [
-						new TerserPlugin({
-							terserOptions: {
-								compress: {
-									comparisons: false
-								},
-								mangle: true,
-								output: {
-									comments: false,
-									ascii_only: true
-								}
-							},
-							parallel: true
-						})
-					],
-					nodeEnv: 'production',
-					concatenateModules: false,
-					splitChunks: {
-						chunks: 'async',
-						minSize: 30000,
-						minRemainingSize: 0,
-						maxSize: 0,
-						minChunks: 1,
-						maxAsyncRequests: 30,
-						maxInitialRequests: 30,
-						enforceSizeThreshold: 50000,
-						cacheGroups: {
-							defaultVendors: {
-								test: /[\\/]node_modules[\\/]/,
-								priority: -10,
-								reuseExistingChunk: true
-							},
-							default: {
-								minChunks: 2,
-								priority: -20,
-								reuseExistingChunk: true
-							}
+				? {}
+				: {
+						minify: {
+							removeComments: true,
+							collapseWhitespace: true,
+							removeRedundantAttributes: true,
+							useShortDoctype: true,
+							removeEmptyAttributes: true,
+							removeStyleLinkTypeAttributes: true,
+							keepClosingSlash: true,
+							minifyJS: true,
+							minifyCSS: true,
+							minifyURLs: true
 						}
-					},
-					runtimeChunk: true
-			  }
-	};
+				  })
+		}),
+		// @ts-ignore
+		new LoadablePlugin(),
+		new CopyWebpackPlugin({
+			// @ts-ignore
+			patterns: [
+				{
+					from: 'src/assets/',
+					to: 'assets/'
+				}
+			]
+		}),
+		new MiniCssExtractPlugin({
+			filename: isDev ? '[name].css' : '[name].[hash].css',
+			chunkFilename: isDev ? '[id].css' : '[id].[hash].css'
+		}),
+		...(isDev
+			? [new ReactRefreshWebpackPlugin()]
+			: [
+					new WebpackPwaManifest({
+						name: process.env.APP_NAME,
+						short_name: process.env.APP_SHORT_NAME,
+						description: process.env.APP_DESCRIPTION,
+						background_color: process.env.APP_BACKGROUND_COLOR,
+						theme_color: process.env.APP_THEME_COLOR,
+						inject: true,
+						ios: true,
+						icons: [
+							{
+								src: r(process.env.APP_ICON),
+								sizes: [72, 96, 128, 144, 192, 384, 512]
+							},
+							{
+								src: r(process.env.APP_ICON),
+								sizes: [120, 152, 167, 180],
+								ios: true
+							}
+						]
+					}),
+					new PrerenderSPAPlugin({
+						staticDir: join(__dirname, 'dist'),
+						routes: Object.values(Routes)
+					})
+			  ])
+	],
+	cache: true,
+	bail: false,
+	target: 'web',
+	devtool: isDev ? 'eval-source-map' : false,
+	devServer: {
+		historyApiFallback: {
+			index: '/'
+		},
+		open: true
+	},
+	stats: 'errors-only',
+	performance: {
+		hints: false
+	},
+	optimization: isDev
+		? {
+				splitChunks: {
+					chunks: 'all'
+				}
+		  }
+		: {
+				minimize: true,
+				minimizer: [
+					new TerserPlugin({
+						terserOptions: {
+							compress: {
+								comparisons: false
+							},
+							mangle: true,
+							output: {
+								comments: false,
+								ascii_only: true
+							}
+						},
+						parallel: true
+					})
+				],
+				nodeEnv: 'production',
+				concatenateModules: false,
+				splitChunks: {
+					chunks: 'async',
+					minSize: 30000,
+					minRemainingSize: 0,
+					maxSize: 0,
+					minChunks: 1,
+					maxAsyncRequests: 30,
+					maxInitialRequests: 30,
+					enforceSizeThreshold: 50000,
+					cacheGroups: {
+						defaultVendors: {
+							test: /[\\/]node_modules[\\/]/,
+							priority: -10,
+							reuseExistingChunk: true
+						},
+						default: {
+							minChunks: 2,
+							priority: -20,
+							reuseExistingChunk: true
+						}
+					}
+				},
+				runtimeChunk: true
+		  }
 };
